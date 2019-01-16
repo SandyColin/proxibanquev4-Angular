98c8217263511377f6cfa9291bf5e8ff2f96f976
SandyColin <sandycolin1990@gmail.com>
2019-01-16 09:51:22 +0100

    getDays() fonctionnant

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -19,11 +19,13 @@ export class SurveyComponent implements OnInit {
   public customer: Customer;
   public isOK: Boolean;
   public calculJours: Number;
+  public number2: Number;
 
   constructor(private service: SurveyService) {
     this.number = 1;
     this.opinion = new Opinion(null, null);
     this.customer = new Customer(undefined);
+    this.number2 = 0;
   }
   // Méthode qui est appelé au moment de la connexion à Angular lorsqu'un sondage est en cours
   // Pour savoir quel sondage est actuellement en cours
@@ -31,7 +33,7 @@ export class SurveyComponent implements OnInit {
     this.survey = this.service.getSurvey();
 
   }
-// Méthode permettant de valider le formulaire lors d'un avis négatif et de créer un objet Opinion (avec facultativement un commentaire)
+  // Méthode permettant de valider le formulaire lors d'un avis négatif et de créer un objet Opinion (avec facultativement un commentaire)
   validateNeg(myForm: NgForm) {
     this.opinion.survey = this.survey;
     this.service.create(this.opinion).subscribe(() => {
@@ -39,7 +41,7 @@ export class SurveyComponent implements OnInit {
     });
     myForm.resetForm(new Opinion(null, null));
   }
-// Méthode permettant de valider le formulaire lors d'un avis positif et de créer un objet Opinion (avec facultativement un numéroClient)
+  // Méthode permettant de valider le formulaire lors d'un avis positif et de créer un objet Opinion (avec facultativement un numéroClient)
   validatePos(myForm: NgForm) {
     this.opinion.survey = this.survey;
     this.service.checkClient(this.customer.clientNumber).subscribe((customer) => {
@@ -47,7 +49,6 @@ export class SurveyComponent implements OnInit {
       this.service.create(this.opinion).subscribe(() => {
         console.log('Avis positif, créé avec succès sur BDD !');
       });
-      this.getDays();
     });
 
     myForm.resetForm(new Opinion(null, null));
@@ -57,27 +58,29 @@ export class SurveyComponent implements OnInit {
     this.number = 3;
     this.opinion.isPositive = false;
   }
-// Methode permettant d'afficher la template pour un avis positif
+  // Methode permettant d'afficher la template pour un avis positif
   onAddPositive() {
     this.number = 2;
     this.opinion.isPositive = true;
   }
-// Methode permettant d'afficher la template de contact après la validation d'un avis positif
-  onNumberValidated() {
-    this.isOK = true;
-  }
+  // Methode permettant d'afficher la template de contact après la validation d'un avis positif
+  // onNumberValidated() {
+  //   this.isOK = true;
+  // }
 
   // Methode permettant de calculer la difference de jour entre la date du jour et la date previsisonnelle de fin de sondage
-  getDays(): Number {
-    console.log(this.survey);
-    const date = new Date(this.survey.provisionalDate[0], this.survey.provisionalDate[1], this.survey.provisionalDate[2]);
+  getDays() {
+    const date = new Date(this.survey.provisionalDate[0], this.survey.provisionalDate[1] - 1, this.survey.provisionalDate[2]);
     console.log(date.getTime());
     const reste = date.getTime() - Date.now();
-    const calculJours = Math.ceil(reste / (1000 * 60 * 60 * 24));
-    const calculDateNow = Math.ceil(Date.now() / (1000 * 60 * 60 * 24));
-    console.log(calculDateNow);
-    console.log(calculJours);
-    return calculJours;
+    this.calculJours = Math.ceil(reste / (1000 * 60 * 60 * 24));
+    console.log(this.calculJours);
+    if (this.calculJours === 0) {
+      this.number2 = 2;
+    } else {
+      this.number2 = 1;
+    }
+
 
   }
 }
